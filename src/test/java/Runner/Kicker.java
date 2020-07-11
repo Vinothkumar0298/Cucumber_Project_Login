807b1bf788e16301c52cd5a40f99cc6056e468bd
Vinothkumar0298 <58282959+Vinothkumar0298@users.noreply.github.com>
2020-07-11 11:46:08 +0530

    Update Kicker.java

--- src/test/java/Runner/Kicker.java
+++ src/test/java/Runner/Kicker.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".\\src\\test\\resources\\Features",glue = "StepDefinition")
+@CucumberOptions(features = "src/test/resources/Features",glue = "StepDefinition")
 public class Kicker {
 
 }
